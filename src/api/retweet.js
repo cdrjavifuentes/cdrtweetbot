14a406bc163f2e4470b6e63734ef882463cca5e5
cdrjavifuentes <cdrjavifuentes@hotmail.com>
2017-10-20 21:54:35 +0000

    reorganizamos el proyecto

--- src/api/retweet.js
+++ src/api/retweet.js
@@ -1,7 +1,7 @@
 // Dependencies =========================
 var
 twit = require('twit'),
-config = require('./config');
+config = require('../config');
 
 var Twitter = new twit(config);
 
@@ -47,53 +47,3 @@ var retweet = function() {
                 }
                 });
 }
-// grab & retweet as soon as program is running...
-console.log('Antes de la función')
-retweet();
-console.log('Despues de función')
-// retweet in every 5 minutes
-setInterval(retweet, 300000);
-
-
-
-// FAVORITE BOT====================
-
-// find a random tweet and 'favorite' it
-var favoriteTweet = function(){
-    var params = {
-    q: '#cdr115',  // REQUIRED
-    result_type: 'recent',
-    lang: 'es'
-    }
-    // find the tweet
-    Twitter.get('search/tweets', params, function(err,data){
-                
-                // find tweets
-                var tweet = data.statuses;
-                var randomTweet = ranDom(tweet);   // pick a random tweet
-                
-                // if random tweet exists
-                if(typeof randomTweet != 'undefined'){
-                // Tell TWITTER to 'favorite'
-                Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
-                             // if there was an error while 'favorite'
-                             if(err){
-                             console.log('CANNOT BE FAVORITE... Error');
-                             }
-                             else{
-                             console.log('FAVORITED... Success!!!');
-                             }
-                             });
-                }
-                });
-}
-// grab & 'favorite' as soon as program is running...
-//favoriteTweet();
-// 'favorite' a tweet in every 5 minutes
-//setInterval(favoriteTweet, 300000);
-
-// function to generate a random tweet tweet
-function ranDom (arr) {
-    var index = Math.floor(Math.random()*arr.length);
-    return arr[index];
-};
