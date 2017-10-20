00dfb5e0b674eed5635989e258272af21c29d453
cdrjavifuentes <cdrjavifuentes@hotmail.com>
2017-10-20 21:11:00 +0000

    return to previous variables

--- config.js
+++ config.js
@@ -6,14 +6,14 @@
  * access_token_secret
  */
 module.exports = {
-//consumer_key: '9YbEZ8n3lX9uZEpBWBWccbKsz',
-//consumer_secret: 'YBqp3CYRp2VakI3vSKidH3btZklxIrDhDRH6ZAZVGyDFmC4rhk',
-//access_token: '921329360501690368-hScRMNw44VSAaF5c7KcZzw4O2cr7aQT',
-//access_token_secret: 'eKL9QJxCXwFkpmV5g5FvmEKDTlp9F9oHA8HVwl5pfX7KC'
+consumer_key: '9YbEZ8n3lX9uZEpBWBWccbKsz',
+consumer_secret: 'YBqp3CYRp2VakI3vSKidH3btZklxIrDhDRH6ZAZVGyDFmC4rhk',
+access_token: '921329360501690368-hScRMNw44VSAaF5c7KcZzw4O2cr7aQT',
+access_token_secret: 'eKL9QJxCXwFkpmV5g5FvmEKDTlp9F9oHA8HVwl5pfX7KC'
 
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+//    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//    access_token: process.env.TWITTER_ACCESS_TOKEN,
+//    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
     
 }
